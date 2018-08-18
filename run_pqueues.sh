9ca16b77624d494ef441f56a11a74d55d6a12d34
Robert Kelly <dakellyfella@gmail.com>
2018-08-18 11:42:28 +0100

    Tweaks to queue size.

--- run_pqueues.sh
+++ run_pqueues.sh
@@ -2,12 +2,16 @@
 
 #  $1 is the pqueue benchmark binary.
 
+
+PQUEUE_SIZE=1280000
+PQUEUE_RANGE=2560000
+
 # Shavit Lotan Priority Queue
-./param_pqueue_benchmark.sh $2 sl_pq leaky 12000000 24000000
-./param_pqueue_benchmark.sh $2 sl_pq retire 12000000 24000000
-./param_pqueue_benchmark.sh $2 c_sl_pq leaky 12000000 24000000
+./param_pqueue_benchmark.sh $2 sl_pq leaky $PQUEUE_SIZE $PQUEUE_RANGE
+./param_pqueue_benchmark.sh $2 sl_pq retire $PQUEUE_SIZE $PQUEUE_RANGE
+./param_pqueue_benchmark.sh $2 c_sl_pq leaky $PQUEUE_SIZE $PQUEUE_RANGE
 
 # SprayList Queue
-./param_pqueue_benchmark.sh $2 spray leaky 12000000 24000000
-./param_pqueue_benchmark.sh $2 sl_pq retire 12000000 24000000
-./param_pqueue_benchmark.sh $2 c_spray leaky 12000000 24000000
+./param_pqueue_benchmark.sh $2 spray leaky $PQUEUE_SIZE $PQUEUE_RANGE
+./param_pqueue_benchmark.sh $2 spray retire $PQUEUE_SIZE $PQUEUE_RANGE
+./param_pqueue_benchmark.sh $2 c_spray leaky $PQUEUE_SIZE $PQUEUE_RANGE
