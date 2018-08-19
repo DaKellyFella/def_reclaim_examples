0810033660be36c0553451d470170c3308b0d899
Robert Kelly <dakellyfella@gmail.com>
2018-08-19 18:50:04 +0100

    Added timeout to pqueue bench.

--- param_pqueue_benchmark.sh
+++ param_pqueue_benchmark.sh
@@ -4,30 +4,30 @@
 
 for t in {1..4}
 do
-  numactl -i 0 ./$1 -d 20 --csv -b $2 -p $3 -t $t -i $4 -r $5
+  timeout --foreground 40s numactl -i 0 ./$1 -d 20 --csv -b $2 -p $3 -t $t -i $4 -r $5
   killall $1
 done
 
 for t in {9..36..9}
 do
-  numactl -i 0 ./$1 -d 20 --csv -b $2 -p $3 -t $t -i $4 -r $5
+  timeout --foreground 40s numactl -i 0 ./$1 -d 20 --csv -b $2 -p $3 -t $t -i $4 -r $5
   killall $1
 done
 
 for t in {45..72..9}
 do
-  numactl -i 0,2 ./$1 -d 20 --csv -b $2 -p $3 -t $t -i $4 -r $5
+  timeout --foreground 40s numactl -i 0,2 ./$1 -d 20 --csv -b $2 -p $3 -t $t -i $4 -r $5
   killall $1
 done
 
 for t in {81..108..9}
 do
-  numactl -i 0,2,1 ./$1 -d 20 --csv -b $2 -p $3 -t $t -i $4 -r $5
+  timeout --foreground 40s numactl -i 0,2,1 ./$1 -d 20 --csv -b $2 -p $3 -t $t -i $4 -r $5
   killall $1
 done
 
 for t in {117..144..9}
 do
-  numactl -i 0,1,2,3 ./$1 -d 20 --csv -b $2 -p $3 -t $t -i $4 -r $5
+  timeout --foreground 40s numactl -i 0,1,2,3 ./$1 -d 20 --csv -b $2 -p $3 -t $t -i $4 -r $5
   killall $1
 done
