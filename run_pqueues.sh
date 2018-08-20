9954f8507c57471b7ed4b2423799e35de9c6b89f
Robert Kelly <dakellyfella@gmail.com>
2018-08-20 13:32:35 +0100

    Fixed typo in script.

--- run_pqueues.sh
+++ run_pqueues.sh
@@ -7,11 +7,11 @@ PQUEUE_SIZE=1280000
 PQUEUE_RANGE=2560000
 
 # Linden Jonsson Queue
-./param_pqueue_benchmark.sh $2 lj_pq leaky $PQUEUE_SIZE $PQUEUE_RANGE
-./param_pqueue_benchmark.sh $2 lj_pq retire $PQUEUE_SIZE $PQUEUE_RANGE
-./param_pqueue_benchmark.sh $2 c_lj_pq leaky $PQUEUE_SIZE $PQUEUE_RANGE
+./param_pqueue_benchmark.sh $1 lj_pq leaky $PQUEUE_SIZE $PQUEUE_RANGE
+./param_pqueue_benchmark.sh $1 lj_pq retire $PQUEUE_SIZE $PQUEUE_RANGE
+./param_pqueue_benchmark.sh $1 c_lj_pq leaky $PQUEUE_SIZE $PQUEUE_RANGE
 
 # SprayList Queue
-./param_pqueue_benchmark.sh $2 spray leaky $PQUEUE_SIZE $PQUEUE_RANGE
-./param_pqueue_benchmark.sh $2 spray retire $PQUEUE_SIZE $PQUEUE_RANGE
-./param_pqueue_benchmark.sh $2 c_spray leaky $PQUEUE_SIZE $PQUEUE_RANGE
+./param_pqueue_benchmark.sh $1 spray leaky $PQUEUE_SIZE $PQUEUE_RANGE
+./param_pqueue_benchmark.sh $1 spray retire $PQUEUE_SIZE $PQUEUE_RANGE
+./param_pqueue_benchmark.sh $1 c_spray leaky $PQUEUE_SIZE $PQUEUE_RANGE
